fbb0e0c28a8fbad45acedf00215cd164651b07bc
Jean Lucca Almeida Condratiuk <jluccacondratiuk@gmail.com>
2020-12-03 16:06:12 -0300

    Adicionado metodologia para coeficientes das equaçoes de esforço cortante e momento fletor

--- models/forces.py
+++ models/forces.py
@@ -3,6 +3,8 @@ class SingleLoad():
         super().__init__()
         self.intensity = intensity
         self.position = position
+        self.shearEquation = {"a":intensity}
+        self.momentEquation = {"b":intensity, "a":intensity*position}
 
     def calcMomento(self, relative_position:float):
         return ((self.intensity)*(self.position-relative_position))
@@ -22,6 +24,8 @@ class ConstantDistributedLoad(DistributedLoad):
     def __init__(self, initial_position:float, final_position:float, intensity:float):
         super(ConstantDistributedLoad, self).__init__(initial_position, final_position)
         self.load = SingleLoad((intensity*(final_position-initial_position)), (((final_position-initial_position)/2)+initial_position))
+        self.shearEquation = {"b":-intensity, "a":(intensity*initial_position)}
+        self.momentEquation = {"c":(-intensity/2),"b":(intensity*initial_position), "a":(-initial_position*initial_position*intensity/2)}
 
 
 class LinearDistributedLoad(DistributedLoad):
@@ -29,29 +33,52 @@ class LinearDistributedLoad(DistributedLoad):
         super(LinearDistributedLoad, self).__init__(initial_position, final_position)
         self.fi = initial_intensity
         self.ff = final_intensity
-        self.load = SingleLoad(((initial_intensity+final_intensity)*(final_position-initial_position)/2),self._calcCentroid()+self.xi)
+        self.trapezoidal = ((initial_intensity*final_intensity)!=0) #Verifica se a carga é triangular ou trapezoidal
+        self.load = SingleLoad(((initial_intensity+final_intensity)*(final_position-initial_position)/2),self._calcCentroid()+self.xi) # Calcula a força pontual equivalente
+        if (self.trapezoidal):                       #Verificação se a carga é trapezoidal ou triangular
+            w = (self.ff-self.fi)/(2*(self.xf-self.xi))
+            self.shearEquation = {"c":-w,"b":-(self.fi - (2*w*self.xi)),"a":-((w*self.xi*self.xi)-(self.fi*self.xi))}
+            w = ((self.ff-self.fi)/(6*(self.xf-self.xi)))
+            self.momentEquation = {"d":-w, "c":-((self.fi/2)-(3*w*self.xi)), "b":-((3*w*self.xi*self.xi)-(self.fi*self.xi)), "a":-((self.fi*self.xi*self.xi/2)-(w*self.xi*self.xi*self.xi))}
+        else:                                        #Bloco de código para cálculo dos coeficientes da equações de momento  de inercia e esforço cortante, para cargas triangulares
+            if (final_intensity-initial_intensity>0):#Verificação se a carga triangular é crescente
+                w = ((self.ff)/(2*(self.xf-self.xi)))
+                self.shearEquation = {"c":(-w),"b":(2*(self.xi)*(w)), "a":-(w*(self.xi)*self.xi)}
+                w = w/3
+                self.momentEquation = {"d":(-w),"c":(w*(3*self.xi)),"b":(w*(-self.xi*self.xi*3)), "a":(w*(self.xi*self.xi*self.xi))}
+            else:                                   #Bloco de código para cálculo dos coeficientes das equação de esforço cortante e momento fletor, para cargas triangulares decrescentes
+                w = ((self.fi)/(2*(self.xf-self.xi)))
+                self.shearEquation = {"c":(w),"b":(w-(2*(self.xi)*(w))), "a":((self.fi*self.xi)+(w*(self.xi)*self.xi))}
+                self.momentEquation = {"d":(w/3),"c":(-(self.fi/2)-(self.xi*w)),"b":((self.xi*self.fi)+(self.xi*self.xi*w)), "a":(-(self.fi*self.xi*self.xi/2)-(self.xi*self.xi*self.xi*w/3))}
 
+                                                    #Função responsável por achar a coordenada x do centroide da carga distribuída
     def _calcCentroid(self):
-        if ((self.fi*self.ff)!=0):
+        if (self.trapezoidal):                      #Condição para verificar se a carga é trapezoidal
             _h1 = min([self.fi, self.ff])
             _area1 = _h1*(self.xf - self.xi)
             _xc1 = (self.xf - self.xi)/2
             _h2 = abs(self.fi-self.ff)
             _area2 = _h2*(self.xf - self.xi)/2
-            if (self.fi-self.ff < 0):
+            if (self.fi-self.ff < 0):               #Condição para verificar se a carga é trapezoidal crescente
                 _xc2 = ((2/3)*(self.xf - self.xi))
-            else:
+            else:                                   #Condição para verificar se a carga é trapezoidal decrescente
                 _xc2 = ((1/3)*(self.xf - self.xi))
             return ((((_area1*_xc1)+(_area2*_xc2))/(_area1+_area2)))
         else:
-            if (self.ff-self.fi < 0):
+            if (self.ff-self.fi < 0):               #Condição para verificar se a carga é triangular crescente
                 return ((2/3)*(self.xf - self.xi))
-            else:
+            else:                                   #Condição para verificar se a carga é triangular decrescente
                 return ((1/3)*(self.xf - self.xi))
 
-# ab = ConstantDistributedLoad(0,3,10)
-# print(ab.load.intensity,ab.load.position)
-# print(ab.xi)
-# print(ab.xf)
-# ab = LinearDistributedLoad(0,6,10,5)
-# print(ab.load.intensity,ab.load.position)
+
+#Código de teste das classes
+ab = ConstantDistributedLoad(3,5,10)
+print(ab.load.intensity,ab.load.position)
+print(f"{ab.shearEquation['b']}x + {ab.shearEquation['a']}")
+print(f"{ab.momentEquation['c']}x² + {ab.momentEquation['b']}x + {ab.momentEquation['a']}")
+print()
+print('---------------------------')
+print()
+ab = LinearDistributedLoad(3,5,5,10)
+print(f"{ab.shearEquation['c']}x² + {ab.shearEquation['b']}x + {ab.shearEquation['a']}")
+print(f"{ab.momentEquation['d']}x³ + {ab.momentEquation['c']}x² + {ab.momentEquation['b']}x + {ab.momentEquation['a']}")
