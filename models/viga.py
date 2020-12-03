a070a4e268f353c0de4ea12f6e0df2d49ef12bdf
Jean Lucca Almeida Condratiuk <jluccacondratiuk@gmail.com>
2020-12-03 16:12:56 -0300

    Pequenas correçoes

--- models/viga.py
+++ models/viga.py
@@ -1,10 +1,10 @@
 class Viga():
-    def __init__(self, length:float, reactions:list, single_loads: list, distributed_loads:list, inertias: list):
+    def __init__(self, length:float, reactions:list, single_loads: list, distributed_loads:list, moments_of_inertia: list):
         self.length = length # cria um atributo à viga, contendo o comprimento da viga
         self.reactions = reactions # cria um atributo, com as reações à serem calculadas, juntamente com suas posições já definidas pelo usuário
         self.single_loads = single_loads # cria um atributo com as forças e posições informadas pelo usuário
         self.distributed_loads = distributed_loads # cria um atributo, com as cargas distribuídas informadas pelo usuário
-        self.inertias = inertias # cria um atributo com os momentos de inércia e suas respectivas posições, também informados pelo usuário
+        self.moment_of_inertia = moments_of_inertia # cria um atributo com os momentos de inércia e suas respectivas posições, também informados pelo usuário
         self.sectionsData = []
         self.forceBalance()
         self._sections()
@@ -30,8 +30,8 @@ class Viga():
             fs+=(self.single_loads[i].intensity)*(self.single_loads[i].position - relative_posi)
         for i in range(len(self.distributed_loads)):
             fs+=self.distributed_loads[i].load.intensity*(self.distributed_loads[i].load.position - relative_posi)
-        for i in range(len(self.inertias)):
-            fs+=self.inertias[i]
+        for i in range(len(self.moment_of_inertia)):
+            fs+=self.moment_of_inertia[i]
         return fs
 
     def _sections(self):
@@ -43,13 +43,14 @@ class Viga():
         for i in self.distributed_loads:
             a.append(i.xi)
             a.append(i.xf)
-        # for i in self.inertias:
+        # for i in self.moment_of_inertia:
         #     a.append(i.position)    
         a = list(set(a))
         a.sort()
         if a[0]==0:
             a.remove(0)
-        a.remove(self.length)
+        if a[-1]==self.length:
+            a.remove(self.length)
         for i in range(len(a)):
             if (i==0):
                 self.sectionsData.append({
@@ -77,29 +78,29 @@ class Viga():
 
 
 #Código de teste
-import forces as rm
-r = [
-    rm.SingleLoad(0,3),
-    rm.SingleLoad(0,10)
-]
-l = [
-    rm.SingleLoad(-10,2),
-    rm.SingleLoad(-20,9)
-]
-d = [
-    rm.LinearDistributedLoad(3,6,-30,-10)
-]
-i = [0,0]
+# import forces as rm
+# r = [
+#     rm.SingleLoad(0,3),
+#     rm.SingleLoad(0,10)
+# ]
+# l = [
+#     rm.SingleLoad(-10,2),
+#     rm.SingleLoad(-20,9)
+# ]
+# d = [
+#     rm.LinearDistributedLoad(3,6,-30,-10)
+# ]
+# i = [0,0]
 
-viga = Viga(10,r,l,d,i)
-print(viga.reactions[0].intensity)
-print(viga.reactions[1].intensity)
-print()
-print('----------------------')
-print('--------Seções--------')
-print('----------------------')
-for i in range(len(viga.sectionsData)):
-    print(f"Seção ${i+1}: ")
-    print()
-    print(f"{viga.sectionsData[i]['position'][0]}<=X<{viga.sectionsData[i]['position'][1]}")
-    print()
+# viga = Viga(10,r,l,d,i)
+# print(viga.reactions[0].intensity)
+# print(viga.reactions[1].intensity)
+# print()
+# print('----------------------')
+# print('--------Seções--------')
+# print('----------------------')
+# for i in range(len(viga.sectionsData)):
+#     print(f"Seção ${i+1}: ")
+#     print()
+#     print(f"{viga.sectionsData[i]['position'][0]}<=X<{viga.sectionsData[i]['position'][1]}")
+#     print()
